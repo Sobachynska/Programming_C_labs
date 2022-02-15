49ab20fce9dd06e17775786a4153e69286b83397
annasobachynska <annasobachynska@gmail.com>
2022-02-15 15:58:20 +0200

    Lab01

--- Lab01/Lab0105/Lab0105.cpp
+++ Lab01/Lab0105/Lab0105.cpp
@@ -5,7 +5,17 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   
+    int a = 0;
+    int b = 0;
+    int pows, abss, res = 0;
+    printf("Enter two int numbers:\n");
+    scanf("%d %d", &a, &b);
+    pows = (pow(a, 2) + pow(b, 2)) / 2;
+    abss = (abs(a) + abs(b)) / 2;
+    res = pows + abss;
+    printf("%d\n", res);
+    return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
