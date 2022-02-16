ee169123dd81266a5c0213002493bc2ba405f4b5
annasobachynska <annasobachynska@gmail.com>
2022-02-16 14:20:41 +0200

    Lab0201

--- Lab01/Lab0201/Lab0201.cpp
+++ Lab01/Lab0201/Lab0201.cpp
@@ -1,1 +1,17 @@
-﻿
+﻿#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+	setlocale(LC_CTYPE, "ukr");
+	float gal, benz, g, l, all;
+	gal = 12776;
+	benz = 45;
+	g = 3.7854;
+	l = 100;
+	all = (((gal * g) / 100) * 45);
+	cout << "all=" << all;
+
+}
