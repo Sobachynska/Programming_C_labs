b4abdf4046e83096a502adec6c9db1308b58ff95
annasobachynska <annasobachynska@gmail.com>
2022-02-16 12:55:59 +0200

    Lab01

--- Lab01/Lab0201/Lab0201.cpp
+++ Lab01/Lab0201/Lab0201.cpp
@@ -1,4 +1,4 @@
-﻿// Lab0101.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿// Lab0105.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
@@ -7,18 +7,10 @@ using namespace std;
 
 int main()
 {
-    int a = 0;
-    int b = 0;
-    int pows, abss, res = 0;
-    cout << "Enter two  integer numbers:";
-    cin >> a;
-    cin >> b;
-    pows = (pow(a, 2) + pow(b, 2)) / 2;
-    abss = (abs(a) + abs(b)) / 2;
-    res = pows + abss;
-    cout << res;
-    return 0;
+    
 }
+    
+
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
