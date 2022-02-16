4f8e2ef131ceb5f001a6c3c39214d9644d969a3e
annasobachynska <annasobachynska@gmail.com>
2022-02-16 13:39:39 +0200

    Lab0504

---
+++ Lab01/Lab0504/Lab0504.cpp
@@ -0,0 +1,15 @@
+﻿#include<cmath>
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "ukr");
+	float z1, z2, alf;
+	cout << "Ввести альфа";
+	cin >> alf;
+	z1 = cos(alf) + cos(2 * alf) + cos(6 * alf) + cos(alf) + cos(7 * alf);
+	z2 = (4 * cos(alf / 2)) * (cos(5 / 2 * alf)) * (cos(4 * alf));
+	cout << "z1=" << z1 << endl;
+	cout << "z2=" << z2;
+}
