ce1c95ba97b4e7301b9d665daa1ead3aabbeed7d
annasobachynska <annasobachynska@gmail.com>
2022-02-21 21:48:58 +0200

    Lab02-0303

---
+++ Lab02/Lab02-0303/Lab02-0303.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    float a, f(a);
+    cin >> a;
+    if (a < 1)
+        f(a) = 0 * a;
+    else
+        if (a < 1)
+            f(a) = sqrt(1 - pow(a, 2));
+        else f(a) = a - 1;
+
+    cout << "f(a)" << f(a);
+
+    return 0;
+}
