adc95d3fdae8dfd43573fe89567fa5b4c78a6a16
annasobachynska <annasobachynska@gmail.com>
2022-02-22 18:10:51 +0200

    Lab02-0401

--- Lab02/Lab02-0401/Lab02-0401.cpp
+++ Lab02/Lab02-0401/Lab02-0401.cpp
@@ -6,26 +6,27 @@ int main()
 {
 	setlocale(LC_CTYPE, "ukr");
 	float F;
-	int a;
-	cout << "Введіть a";
+	float a;
+	cout << "Введіть a ";
 	cin >> a;
-	int b;
-	cout << "Введіть b";
+	float b;
+	cout << "Введіть b ";
 	cin >> b;
-	int c;
-	cout << "Введіть c";
+	float c;
+	cout << "Введіть c ";
 	cin >> c;
-	int x;
-	cout << "Введіть x";
+	float x;
+	cout << "Введіть x ";
 	cin >> x;
-	if ((x < 0 and b != 0) {
-		F = -a * pow(x, 3) - b;
+	if ((x < 0 and b != 0)) {
+		F = (a * pow(x, 2) + b);
 	}
-	else if ((x + c) > 0 and a != 0) {
-		F = (x - a) / (x - c);
-	}
-	else {
-		F = (x / c) + (c / x);
-	}
-	cout << "F=" << F;
+	else if ((x > 0) and (b = 0)) {
+			F = (x - a) / (x - c);
+		}
+	
+		else {
+			F = (x / c);
+		}
+	cout << "F = " << F;
 }
