8059ee755e94d052a110e8b2982c00e4a0bedc56
annasobachynska <annasobachynska@gmail.com>
2022-02-23 20:26:52 +0200

    Lab02-0611

--- Lab02/Lab02-0611/Lab02-0611.cpp
+++ Lab02/Lab02-0611/Lab02-0611.cpp
@@ -6,15 +6,6 @@ using namespace std;
 int main() 
 {
 	setlocale(LC_CTYPE, "ukr");
-	int n, i, a;
 	
-cout << "Введiть цiле число: ";
-cin >> n;
-for (i = 1; i <= n - 1; i++)
-a *= 2; //*= множення з переприсвоєнням
-cout << "Результат: " << a;
-return 0;
-
-
 	return 0;
 	}
