0b82a6292ee13f660db476ec01fea35693a0a56a
annasobachynska <annasobachynska@gmail.com>
2022-02-23 20:25:40 +0200

    Lab02

--- Lab02/Lab02-0110/Lab02-0110.cpp
+++ Lab02/Lab02-0110/Lab02-0110.cpp
@@ -1,19 +1,38 @@
 ﻿//10. Дано три дійсних числа X, Y, Z.Перерозподілити значення так, щоб в X виявилося найменше
 //з цих значень, а у Z – найбільше.
 
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <cmath>
 
-using namespace std;
-
-int main() 
-{
-	setlocale(LC_CTYPE, "ukr");
-	float x, y, z, max, min = 0;
-	cout << "Введіть три дійсних числа через пробіл: ";
-	cin >> x >> y >> z;
-	if 
-
-   
-}
+    using namespace std;
+    int main()
+    {
+        setlocale(LC_CTYPE, "ukr");
+        int X, Y, Z;
+        cout << "Введiть X: ";
+        cin >> X;
+        cout << "Введiть Y: ";
+        cin >> Y;
+        cout << "Введiть Z: ";
+        cin >> Z;
+        if (X > Y)
+        {
+            swap(X, Y); // міняє місцями
+        }
+        if (Z < Y)
+        {
+            swap(Y, Z);
+        }
+        if (X > Z)
+        {
+            swap(X, Z);
+        }
+        if (X > Y)
+        {
+            swap(X, Y);
+        }
+        cout << "X=" << X << endl;
+        cout << "Y=" << Y << endl;
+        cout << "Z=" << Z;
+        return 0;
+    }
--- Lab02/Lab02-0611/Lab02-0611.cpp
+++ Lab02/Lab02-0611/Lab02-0611.cpp
@@ -6,12 +6,15 @@ using namespace std;
 int main() 
 {
 	setlocale(LC_CTYPE, "ukr");
-	int i, n, a;
-	a = 2;
-	cout << "Введiть цiле число: ";
-	cin >> n;
-	for (i = 1; i <= n - 1; i++) 
-		a *= 2; //*= множення з переприсвоєнням
-	cout << "Результат: " << a;
+	int n, i, a;
+	
+cout << "Введiть цiле число: ";
+cin >> n;
+for (i = 1; i <= n - 1; i++)
+a *= 2; //*= множення з переприсвоєнням
+cout << "Результат: " << a;
+return 0;
+
+
 	return 0;
 	}
