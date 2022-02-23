cbf4d27aca91330aed8492443d2fab0dac6e9f22
annasobachynska <annasobachynska@gmail.com>
2022-02-23 20:29:19 +0200

    Lab02-0205

--- Lab02/Lab02-0205/Lab02-0205.cpp
+++ Lab02/Lab02-0205/Lab02-0205.cpp
@@ -14,7 +14,7 @@ int main()
     v2 = sqrt((1 / (pow(a, 2) + 1)) + (1 / (pow(b, 2) + 1)) + (1 / (pow(c, 2) + 1)));
     min = v1;
     if (v1 > v2)
-    cout << "min = " << v1;
-    else cout << "min = " << v2;
+        cout << "min = " << v2;
+    else cout << "min = " << v1;
     return 0;
 }
