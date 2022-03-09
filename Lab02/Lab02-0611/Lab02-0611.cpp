0c13b8dcb4ad98614894382c0acdfa79efa3a876
annasobachynska <annasobachynska@gmail.com>
2022-03-09 19:32:13 +0200

    Lab02

--- Lab02/Lab02-0611/Lab02-0611.cpp
+++ Lab02/Lab02-0611/Lab02-0611.cpp
@@ -1,25 +1,23 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 #include <math.h>
 
 using namespace std;
 
 int main()
 {
-	setlocale(LC_CTYPE, "ukr");
-	double sum,  i , z;
-	int n;
-	cin >> n;
-	sum = 0;
-	z = 0;
-	for (i = 1; n>=i;++i)
-	{
-		z = z + sin(i);
-		sum = sum + 1 / z;
+    setlocale(LC_CTYPE, "ukr");
+    double sum, i, z;
+    int n;
+    cin >> n;
+    sum = 0;
+    z = 0;
+    for (i = n; i >= 1; i--)
+    {
+        z = z + sin(i);
+        sum = sum + 1 / z;
+    }
+    cout << sum;
 
-	}
-	cout << sum;
-
-		return 0;
+    return 0;
 }
-
-	
