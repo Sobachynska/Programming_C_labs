62c96b5714c5f9c5af3bbdb6a68989a1b8bc4772
annasobachynska <annasobachynska@gmail.com>
2022-03-10 15:24:07 +0200

    Lab02-0810

---
+++ Lab02/Lab02-0810/Lab02-0810.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    int n, k;
+    float x, z;
+    if (i = 2; x = cos(z); n--)
+    {
+        k = k + 1;
+        z = z + a[i];
+    }
+    cout << k;
+
+    return 0;
+}
