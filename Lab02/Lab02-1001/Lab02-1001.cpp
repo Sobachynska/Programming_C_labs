7710fa41de6b76bda9afcf46cea1bb44d5745d07
annasobachynska <annasobachynska@gmail.com>
2022-03-12 15:44:32 +0200

    Lab02-1001

---
+++ Lab02/Lab02-1001/Lab02-1001.cpp
@@ -0,0 +1,27 @@
+﻿//i від 1 до 100
+//j від 1 до 50
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+   
+    int i, j;
+    double a=0;
+    cout << "Ведіть i: ";
+    cin >> i;
+    cout << "Ведіть j: ";
+    cin >> j;
+    for (i = 1; i <= 100; i++) {
+        for (j = 1; j <= 50; j++);
+            a = (i + j); 
+    }
+    cout << "Результат: " << a;
+
+
+    return 0;
+}
+
+
