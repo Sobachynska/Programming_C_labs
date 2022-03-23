8912419bb90eb1c1128e7509f1d3b837a0c685e6
annasobachynska <annasobachynska@gmail.com>
2022-03-23 10:16:10 +0200

    Lab02-0907

---
+++ Lab02/Lab02-0907/Lab02-0907.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int x0 = 0, x1 = 1, x, n, tmp1, tmp2;
+    tmp1 = x0;
+    tmp2 = x1;
+
+    std::cin >> n;
+
+    for (int i = 3; i <= n; i++)
+    {
+        x = tmp2 * (tmp1 + 1);
+        tmp1 = tmp2;
+        tmp2 = x;
+    }
+
+    std::cout << x << "\n";
+}
+
