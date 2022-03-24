18c16640b2333625f44fdd542cb380344baaa8b0
annasobachynska <annasobachynska@gmail.com>
2022-03-24 23:18:16 +0200

    Lab02-0705

--- Lab02/Lab02-0705/Lab02-0705.cpp
+++ Lab02/Lab02-0705/Lab02-0705.cpp
@@ -1,26 +1,24 @@
-﻿//Разработаем ещё одну программу на обработку одномерного массива в С++.
-//Программа должна последовательно считывать десять введённых чисел с клавиатуры.
-//Все введённые числа  просуммировать, результат вывести на экран.
+﻿#include <iostream>
+#include <math.h>
 
-// array_sum.cpp: определяет точку входа для консольного приложения.
-
-#include "math.h"
-#include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[])
+int main()
 {
-    int array1[10]; // объявляем целочисленный массив
-    cout << "Enter elementi massiva: " << endl;
-    int sum = 0;
-    for (int counter = 0; counter < 10; counter++) // цикл для считывания чисел
-        cin >> array1[counter];  // считываем вводимые с клавиатуры числа
-    cout << "array1 = {";
-    for (int counter = 0; counter < 10; counter++) // цикл для вывода элементов массива
-        cout << 
-    for (int counter = 0; counter < 10; counter++) // цикл для суммирования чисел массива
-        sum += array1[counter]; // суммируем элементы массива
-    cout << "}\nsum = " << sum << endl;
-    
+    int i, answer = 0;
+    int a;
+    cout << "input i= ";
+    cin >> i;
+    for (int j = 0; j < i; j++) {
+
+        cout << "Enter a" << j + 1 << "= ";
+        cin >> a; 
+
+        answer = answer + abs(a);
+
+    }
+    cout << answer; 
+
+
     return 0;
 }
