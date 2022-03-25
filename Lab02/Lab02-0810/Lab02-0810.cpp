1dfcb759978e6170b796f6f16885593c5787fb12
annasobachynska <annasobachynska@gmail.com>
2022-03-25 19:41:27 +0200

    Lab02-0810

--- Lab02/Lab02-0810/Lab02-0810.cpp
+++ Lab02/Lab02-0810/Lab02-0810.cpp
@@ -5,45 +5,19 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_CTYPE, "ukr");
-    int n,size, arr,i,kx;
-    float x, z;
-  
-    cout << "Введіть n: ";
-    cin >> size;
-     kx = 0;
-     
-    cout << "Введіть масив чисел : ";
-    cin >> arr[size];
-    int* arr = new int[size];
-    int arr[size];
-    for (int i = 0; i < n; i++) {
-        cout << "[" << i + 1 << "]" << ": ";
-        cin >> arr[size];
-    }
-   
-}
-   
+    int n, answer = 0;
+    double a;
+    cout << "input n= ";
+    cin >> n; 
+    for (int j = 0; j < n; j++) {
 
-    
-//        cin >> x;
-//        z = a[1];
-//
-//        for (i=2; x=cos(z);n--) 
-//        {
-//
-//            if (x = cos(z)) {
-//
-//                kx = kx + 1;
-//                z = z + a[i];
-//            }
-//        }
-//
-//    //if (x = cos(z)){
-//    //   
-//    //    kx = kx + 1;
-//    //    z = z + a[i];
-//    //}
-//    cout << kx;
-//}
-   
+        cout << "Enter a" << j + 1 << "= ";
+        cin >> a; 
+
+        answer = answer + cos(a);
+
+    }
+    cout << answer; 
+
+    return 0;
+}
