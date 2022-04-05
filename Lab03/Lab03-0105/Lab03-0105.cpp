63b77515490b6c4a0239f343e826113b628a78ea
annasobachynska <annasobachynska@gmail.com>
2022-04-05 13:37:45 +0300

    Lab03-0105

---
+++ Lab03/Lab03-0105/Lab03-0105.cpp
@@ -0,0 +1,22 @@
+﻿#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a, b;
+    cout << "vvedit a ";
+    cin >> a;
+    cout << "vvedit b ";
+    cin >> b;
+    char* aPtr = &a, * bPtr = &b;
+
+    *aPtr = a + 1;
+    cout << "zminene znachenia a: " << *aPtr << endl;
+
+    cout << "a=" << *bPtr;
+    cout << " b=" << *aPtr;
+
+    return 0;
+}
