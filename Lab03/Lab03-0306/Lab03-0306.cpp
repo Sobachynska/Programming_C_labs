fef78360bc505ee562426841f58729b28759d613
annasobachynska <annasobachynska@gmail.com>
2022-04-18 23:30:29 +0300

    Lab03-0306

---
+++ Lab03/Lab03-0306/Lab03-0306.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+
+    int* aPrt = new int;
+    cin >> *aPrt;
+    int* bPrt = new int;
+    cin >> *bPrt;
+    int* cPrt = new int;
+    cin >> *cPrt;
+}
