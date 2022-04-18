18fac4ebca85b37efc5228a5875cb5fd1182c0df
annasobachynska <annasobachynska@gmail.com>
2022-04-18 23:26:38 +0300

    Lab03-0216

---
+++ Lab03/Lab03-0216/Lab03-0216.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int r = 6, h = 7;
+	int V;
+	int  P = 3; //приблизне значення числа Пі
+	int* rPtr = &r, * hPtr = &h, * VPtr = &V;
+	*VPtr = P * (*rPtr) * (*rPtr);
+	cout << "V=" << *VPtr;
+
+}
