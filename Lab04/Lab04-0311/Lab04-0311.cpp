914ea520cb90ab8c1992750115690ffebef7f96e
annasobachynska <annasobachynska@gmail.com>
2022-04-27 22:35:05 +0300

    Lab04-0311

---
+++ Lab04/Lab04-0311/Lab04-0311.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    int p = 5, i, j;
+    float x;
+    int min, max;
+
+    float arr[5] = { 1.0, 5.8, 23.4, 54.3, 57.9 };
+    cout << "введiть x: ";
+    cin >> x;
+    for (int a = 0; a < p - 1; a++) {
+
+        if (arr[a] > x) {
+            min = a - 1;
+            max = a;
+            cout << min << endl;
+            cout << max << endl;
+            break;
+        }
+    }
+}
