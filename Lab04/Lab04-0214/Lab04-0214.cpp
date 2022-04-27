9a61f10dfa1c71c051b0529572660c21b85e6848
annasobachynska <annasobachynska@gmail.com>
2022-04-27 15:54:09 +0300

    Lab04-0214

---
+++ Lab04/Lab04-0214/Lab04-0214.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int n = 10;
+    int A[n] = {};
+    for (int i = 0; i < n; i++)
+    {
+        cout << "A[" << i << "] = ";
+        cin >> A[i];
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] != A[i - 1])
+        {
+            count = count + 1;
+        }
+    }
+    cout << "kilkist riznyh" << count;
+}
