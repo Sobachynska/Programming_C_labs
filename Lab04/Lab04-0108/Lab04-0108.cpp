c91a9ef225985237cf13dff558bfce010da53945
annasobachynska <annasobachynska@gmail.com>
2022-04-27 15:51:45 +0300

    Lab04-0108

---
+++ Lab04/Lab04-0108/Lab04-0108.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	long long int n, a, a1 = 1, a2 = 1, a3 = 2, a4 = 3;
+
+	cin >> n;
+	for (int k = 5; k <= n; k++) {
+		a = a3 * a4;
+		a3 = a4;
+		a4 = a;
+	}
+	cout << a ;
+
+}
