ce5efdfe5ad75dea64f138ef338320e93eb93a88
annasobachynska <annasobachynska@gmail.com>
2022-04-27 15:47:24 +0300

    Lab03-0306

--- Lab03/Lab03-0306/Lab03-0306.cpp
+++ Lab03/Lab03-0306/Lab03-0306.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 int main()
 {
-    int a, b, c;
+    int a, b, c, d;
 
     int* aPrt = new int;
+    cout << "znachenya a ";
     cin >> *aPrt;
     int* bPrt = new int;
+    cout << "znachenya b ";
     cin >> *bPrt;
     int* cPrt = new int;
+    cout << "znachenya c ";
     cin >> *cPrt;
+
+    d = *aPrt / 2;
+    cout << d;
 }
