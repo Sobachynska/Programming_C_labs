4b2250c5cd476a7d0ee4bf56b8edd9b5bd5d8087
annasobachynska <annasobachynska@gmail.com>
2022-05-01 23:28:21 +0300

    Lab04-0405

--- Lab04/Lab04-0405/Lab04-0405.cpp
+++ Lab04/Lab04-0405/Lab04-0405.cpp
@@ -1,10 +1,24 @@
-﻿#include <iostream>
+﻿int i, j, x;
+const int n = 3;
+int M[n][n] = {
+  {3, 5, 6},
+  {4, 5, 6},
+  {5, 7, 8}
+};
 
-int main()
-{
-    setlocale(LC_CTYPE, "ukr");
-
-    const int n = 5;
-    int A[i][j];
+for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+        cout << M[i][j] << " ";
+    }
+    cout << "\n";
+}
+int c = 9;
+x = 0;
+for (i = 0; i < n; j++) {
+    if (M[i][n - 1 - i] < c) {
+        x = M[i][n - i]//незнаю як записати що елементи перемножаються
+    };
+    cout << x;
+    return 0;
 
 }
