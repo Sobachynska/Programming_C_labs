523b40f1f2700e563fc172d915684a6330851f4d
annasobachynska <annasobachynska@gmail.com>
2022-05-05 11:19:23 +0300

    Lab04-0705

--- Lab04/Lab04-0705/Lab04-0705.cpp
+++ Lab04/Lab04-0705/Lab04-0705.cpp
@@ -1,21 +1,17 @@
-﻿
-#include <iostream>
-
+﻿#include <iostream>
 using namespace std;
 
 int main()
 {
     const int N = 5;
     int matrix[N][N];
-
-    // Заполняем матрицу случайными числами.
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             matrix[i][j] = rand() % 20 - 10;
 
-    int sum[N], k = 0;  // sum - Массив, в котором будет хранится сума нужных строк. k -  кол-во строк с эл. на главной диагонале, которые меньше нуля. 
+    int max[N], matrix[j], k = 0;
     for (int i = 0; i < N; i++)
-        sum[i] = 0;
+        max[i] = 0;
 
     for (int i = 0; i < N; i++)
     {
@@ -23,11 +19,12 @@ int main()
         {
             k++;
             for (int j = 0; j < N; j++)
-                sum[i] += matrix[i][j];  // Просуммировать все эл. строки в i-й эл. массива суммы.
+                for (int r = 1; r < N; r++)
+                {
+                    if (max < matrix[j]) max = matrix[j];
+                }
         }
     }
-
-    // Вывод матрицы и результата.
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
@@ -38,7 +35,7 @@ int main()
     for (int i = 0; i < k; i++)
     {
         if (matrix[i][i] < 0)
-            cout << "Sum of row #" << i << " = " << sum[i] << endl;
+            cout << "max element: " << i << " = " << max[i] << endl;
     }
 
     system("pause");
