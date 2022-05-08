b26f8576d08858017ed867b16c0679b60448b564
annasobachynska <annasobachynska@gmail.com>
2022-05-08 23:24:29 +0300

    Lab05

---
+++ Lab05/Lab05-0105/Lab05-0105.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    cout << "Введiть текст англiйською:";
+    string str;
+    getline(cin, str);
+    int j = 0;
+
+    for (int i = str.size() - 1; i > 0; i--)
+        if (str[i] == '.' && str[i - 1] == '.') str.erase(i, 1);
+
+    cout << str << endl;
+    system("pause");
+    return 0;
+}
---
+++ Lab05/Lab05-0311/Lab05-0311.cpp
@@ -0,0 +1,35 @@
+﻿    string sn, s: ;
+	integer l, k, kod;
+	real v, p;
+	cout << "Введіть рядок ";
+	getline(cin, s);
+    l = length(s);
+    p = 1; 
+	k = 1;
+	repeat sn : = '';
+	while (s[k] > = '0') and (s[k] <= '9') and (k <= l) do begin sn : = sn + s[k];
+    k: = k + 1; end;
+	if sn < > '' then begin val(sn, v, kod);
+    p: = p * v; end;
+    k: = k + 1;
+	until k > l;
+	writeln('p =', p); end.
+    return 0;
+}
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "ukr");
+	string sn, s;
+	int l, k, kod;
+	double v, p;
+	cout << "Введіть рядок ";
+	getline(cin, s);
+	l = length(s) const;
+	p = 1;
+	k = 1;
+		return 0;
+}
